1d48fd089af83ece93a5b7a9a3a49ac35bebe902
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-10 16:47:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,26 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
+  float a=0, b=0; c=0, triangolo=0, quadrato=0, rettangolo=0, trapezio=0;;
 
 cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
 
+    triangolo=a*b/2;
+    cout<<triangolo<<endl;
+  
+    quadrato=0;
+    S=a*a;
+    cout<<quadrato<<endl;
+  
+    rettangolo=0;
+    S=a*b;
+    cout<<rettangolo<<endl;
+  
+    trapezio=0;
+    S=(a+b)*c/2;
+    cout<<trapezio<<endl;
+  return 0;
 
 }
